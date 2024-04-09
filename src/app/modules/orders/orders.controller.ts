71873fc0d4e4c5ae4c8f3ac2670f64282d45fc4e
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-04-09 22:44:46 +0600

    refactor: Added catchAsync middleware to maintain asynchronous routing and error handling

--- src/app/modules/orders/orders.controller.ts
+++ src/app/modules/orders/orders.controller.ts
@@ -1,72 +1,79 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { OrdersServices } from "./orders.service";
 import sendResponse from "../../utilities/sendResponse";
 import { IOrders } from "./orders.interface";
 import httpStatus from "http-status";
+import catchAsync from "../../utilities/catchAsync";
 
-const createOrder = async (req: Request, res: Response) => {
-  try {
-    const result = await OrdersServices.ADD_ORDER_TO_DB(req.body);
+const createOrder: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    try {
+      const result = await OrdersServices.ADD_ORDER_TO_DB(req.body);
+      sendResponse<IOrders>(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "New order created",
+        data: result,
+      });
+    } catch (error) {}
+  }
+);
+
+const getOrdersList: RequestHandler = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await OrdersServices.GET_ORDERS_FROM_DB();
+    sendResponse<IOrders[]>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "All orders data retrieved",
+      data: result,
+    });
+    next();
+  }
+);
+
+const partiallyUpdateOrderDetails: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const result = await OrdersServices.PATCH_ORDER_TO_DB(id, updatedData);
+
     sendResponse<IOrders>(res, {
       statusCode: httpStatus.OK,
       success: true,
-      message: "New order created",
+      message: `Order Information for id:${id} updated`,
       data: result,
     });
-  } catch (error) {}
-};
+  }
+);
 
-const getOrdersList = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const result = await OrdersServices.GET_ORDERS_FROM_DB();
-  sendResponse<IOrders[]>(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: "All orders data retrieved",
-    data: result,
-  });
-  next();
-};
+const updateAndReplaceOrderDetails: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const result = await OrdersServices.PUT_ORDER_TO_DB(id, updatedData);
+    sendResponse<IOrders>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: `Order Information of id:${id} updated and replaced`,
+      data: result,
+    });
+  }
+);
 
-const partiallyUpdateOrderDetails = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const updatedData = req.body;
-  const result = await OrdersServices.PATCH_ORDER_TO_DB(id, updatedData);
+const deleteOrder: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const result = await OrdersServices.DELETE_ORDER_FROM_DB(id);
 
-  sendResponse<IOrders>(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: `Order Information for id:${id} updated`,
-    data: result,
-  });
-};
-
-const updateAndReplaceOrderDetails = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const updatedData = req.body;
-  const result = await OrdersServices.PUT_ORDER_TO_DB(id, updatedData);
-  sendResponse<IOrders>(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: `Order Information of id:${id} updated and replaced`,
-    data: result,
-  });
-};
-
-const deleteOrder = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const result = await OrdersServices.DELETE_ORDER_FROM_DB(id);
-
-  sendResponse<IOrders>(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: `Order with id ${id} has been deleted`,
-    data: result,
-  });
-};
+    sendResponse<IOrders>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: `Order with id ${id} has been deleted`,
+      data: result,
+    });
+  }
+);
 
 export const OrdersController = {
   createOrder,
